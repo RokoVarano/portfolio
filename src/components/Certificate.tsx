a96d25f43cd1cc3ca54bf6295bf7486609d3cae5
Rodrigo <rodrigo.ibaceta01@gmail.com>
2021-12-13 15:13:07 -0300

    Implement popup + carousel

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
 import defaultPic from '../Utils';
+import CertificateDetails from './CertificateDetails';
 
 interface Props {
   pictureUrl?: string,
@@ -7,13 +9,40 @@ interface Props {
   title?: string,
 }
 const Certificate = ({ pictureUrl, title, icon }: Props) => (
-  <div className="certificate-component">
-    <div className="picture" style={{ backgroundImage: `url(${pictureUrl})` }} />
-    <div className="footnote">
-      <div className="icon" style={{ backgroundImage: `url(${icon})` }} />
-      <h3 className="title">{title}</h3>
-    </div>
-  </div>
+  <Popup
+    trigger={(
+      <div className="certificate-component">
+        <div className="picture" style={{ backgroundImage: `url(${pictureUrl})` }} />
+        <div className="footnote">
+          <div className="icon" style={{ backgroundImage: `url(${icon})` }} />
+          <h3 className="title">{title}</h3>
+        </div>
+      </div>
+    )}
+    modal
+    nested
+  >
+    <CertificateDetails items={
+      [
+        {
+          picture: defaultPic,
+          source: 'Microverse',
+          date: 'December, 2021',
+        },
+        {
+          picture: defaultPic,
+          source: 'Microverse',
+          date: 'December, 2022',
+        }, {
+          picture: defaultPic,
+          source: 'Microverse',
+          date: 'December, 2023',
+        },
+      ]
+    }
+    />
+  </Popup>
+
 );
 
 Certificate.defaultProps = {
