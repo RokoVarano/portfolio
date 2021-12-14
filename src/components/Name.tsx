fad7e36734c687e54200e99288d999bbbcd9f32c
Rodrigo <rodrigo.ibaceta01@gmail.com>
2021-12-14 11:16:38 -0300

    Name component

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 
 const Name:FC = () => (
   <div id="name-component">
+    <div id="name-flag" />
     <h1>Rodrigo Ibaceta</h1>
     <h2>Full-stack web developer</h2>
   </div>
