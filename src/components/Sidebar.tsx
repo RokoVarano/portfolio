6fd7e12b6dc732aa71d2c79b3af87d5dde7b8e74
Rodrigo <rodrigo.ibaceta01@gmail.com>
2021-12-22 13:50:34 -0300

    Animate sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React, {
+  FC, useEffect, useRef, useState,
+} from 'react';
 
-const Sidebar = () => (
-  <nav id="sidebar-component">
-    <div id="top-separator" />
+const Sidebar:FC = () => {
+  const homeRef = useRef<HTMLLIElement>(null);
+  const projectsRef = useRef<HTMLLIElement>(null);
+  const certificatesRef = useRef<HTMLLIElement>(null);
+  const recomendationsRef = useRef<HTMLLIElement>(null);
+  const contactRef = useRef<HTMLLIElement>(null);
+  const resumeRef = useRef<HTMLLIElement>(null);
 
-    <ul>
-      <li><a href="#home"><div className="bluebar"><h4>Home</h4></div></a></li>
-      <li><a href="#projects"><div className="bluebar"><h4>Projects</h4></div></a></li>
-      <li><a href="#certificates"><div className="bluebar"><h4>Certificates</h4></div></a></li>
-      <li><a href="#recomendations"><div className="bluebar"><h4>Experience</h4></div></a></li>
-      <li><a href="#contact"><div className="bluebar"><h4>Contact</h4></div></a></li>
-      <li><a href="#resume"><div className="bluebar"><h4>Resume.pdf</h4></div></a></li>
-    </ul>
-  </nav>
-);
+  const [currentRef, setCurrentRef] = useState<React.RefObject<HTMLLIElement>>();
 
+  useEffect(() => {
+    if (currentRef === undefined) return;
+
+    const refs = [homeRef, projectsRef, certificatesRef, recomendationsRef, contactRef, resumeRef];
+
+    refs.forEach((ref) => ref.current?.classList.remove('turn-on'));
+
+    currentRef.current?.classList.add('turn-on');
+  }, [currentRef]);
+
+  return (
+
+    <nav id="sidebar-component">
+      <div id="top-separator" />
+
+      <ul>
+        <li ref={homeRef} onClick={() => setCurrentRef(homeRef)}><a href="#home"><h4>Home</h4></a></li>
+        <li ref={projectsRef} onClick={() => setCurrentRef(projectsRef)}><a href="#projects"><h4>Projects</h4></a></li>
+        <li ref={certificatesRef} onClick={() => setCurrentRef(certificatesRef)}><a href="#certificates"><h4>Certificates</h4></a></li>
+        <li ref={recomendationsRef} onClick={() => setCurrentRef(recomendationsRef)}><a href="#recomendations"><h4>Experience</h4></a></li>
+        <li ref={contactRef} onClick={() => setCurrentRef(contactRef)}><a href="#contact"><h4>Contact</h4></a></li>
+        <li ref={resumeRef} onClick={() => setCurrentRef(resumeRef)}><a href="#resume"><h4>Resume.pdf</h4></a></li>
+      </ul>
+    </nav>
+  );
+};
 export default Sidebar;
